54a6d5009b55dfeccbb3a1ef7c595063f32c8ac4
ArthurOReis <arthuroreis2005@gmail.com>
2023-08-22 10:43:11 -0300

    Adicionando link

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -2,20 +2,20 @@ import logo from './logo.png';
 import styles from './Navbar.module.css';
 
 export default function Navbar() {
-  
-  const opcoes = ["Início", "Planos", "Quem somos", "Suporte"];
-  
+    
   return (
     <nav className={styles.navbar}>
         <img src={logo} className={styles.logo} alt="Logo da Gourmet Express"/>
         <ul className={styles.containerNavbar}>
-            {opcoes.map(opcao => (
-                <>
-                    <a href="https://github.com/SmartCodeGenius" key={opcao.indexOf()} className={styles.link}>{opcao}</a>
-                    <div className={styles.barraNavbar}></div>
-                </>
-            ))}
-            <a className={styles.entrar}>Entrar</a>
+            <a href="https://github.com/ArthurOReis" className={styles.link}>Início</a>
+            <div className={styles.barraNavbar}></div>
+            <a href="https://github.com/NicolasSNichnig" className={styles.link}>Planos</a>
+            <div className={styles.barraNavbar}></div>
+            <a href="https://github.com/PenRoseRubix" className={styles.link}>Quem somos</a>
+            <div className={styles.barraNavbar}></div>
+            <a href="https://github.com/HeitorWestphal" className={styles.link}>Suporte</a>
+            <div className={styles.barraNavbar}></div>
+            <a className={styles.entrar} href="https://github.com/SmartCodeGenius">Entrar</a>
         </ul>
     </nav>
   );
