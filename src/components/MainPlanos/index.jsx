1eaceebcedae5aac345decf70799815967a68f3c
ArthurOReis <121455711+ArthurOReis@users.noreply.github.com>
2023-09-18 11:45:12 -0300

    Estilizando Planos

--- src/components/MainPlanos/index.jsx
+++ src/components/MainPlanos/index.jsx
@@ -2,11 +2,16 @@ import styles from './MainPlanos.module.css'
 import cereja from './cereja.svg'
 import tomate from './tomate.svg'
 import caixote from './caixote.svg'
+import { useState } from 'react'
 
 export default function MainPlanos() {
-  const planos = [{nome:'Plano Cereja', id:1, preco:35, selecionado:false, icone:cereja },
-                  {nome:'Plano Tomate', id:2, preco:70, selecionado:false, icone:tomate }, 
-                  {nome:'Plano Caixote', id:3, preco:105, selecionado:false, icone:caixote }];
+  const [ativo, setAtivo] = useState([false, false, false]);
+
+  const planos = [{nome:'Plano Cereja', id:1, preco:35, selecionado:ativo[0], icone:cereja },
+                  {nome:'Plano Tomate', id:2, preco:70, selecionado:ativo[1], icone:tomate }, 
+                  {nome:'Plano Caixote', id:3, preco:105, selecionado:ativo[2], icone:caixote }];
+
+  
 
   return (
     <section className={styles.container}>
@@ -14,10 +19,15 @@ export default function MainPlanos() {
       <div className={styles.planosContainer}>
         {planos.map(plano => (
             <div className={styles.opcao} onClick={() => alert(plano.id)}>
-            <h2>{plano.nome}</h2>
-            <h1>R${plano.preco}</h1>
-            <h3>por mês</h3>
-            <img src={plano.icone} alt="" style={styles.iconeImagem}/>
+            <h2 className={styles.nomePlano} unselectable='on'>{plano.nome}</h2>
+            <h1 className={styles.precoPlano} unselectable='on'>R${plano.preco}</h1>
+            <h3 className={styles.porMes}>por mês</h3>
+            {plano.icone === cereja ? 
+            <img src={plano.icone} alt="" className={styles.iconeCereja}/> : <></>}
+             {plano.icone === tomate ? 
+            <img src={plano.icone} alt="" className={styles.iconeTomate}/> : <></>}
+             {plano.icone === caixote ? 
+            <img src={plano.icone} alt="" className={styles.iconeCaixote}/> : <></>}
             </div>
         ))}
       </div>
